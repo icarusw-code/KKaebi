9812a6cd2b2583e1dc1f42c88f892fca1426e50e
SeongJin Choi <77170611+icarusw-code@users.noreply.github.com>
2022-10-12 11:39:12 +0900

    update data

--- Assets/World.multiplay/IngredientData.ts
+++ Assets/World.multiplay/IngredientData.ts
@@ -3,275 +3,328 @@ export const IngredientData : {[key : string] : any} = {
        {
           "name": "고추장",
           "id": 1001,
-          "type": "조미료"
+          "type": "조미료",
+          "count": 0
        },
        {
           "name": "된장",
           "id": 1002,
-          "type": "조미료"
+          "type": "조미료",
+          "count": 0
        },
        {
           "name": "간장",
           "id": 1003,
-          "type": "조미료"
+          "type": "조미료",
+          "count": 0
        },
        {
           "name": "조청",
           "id": 1004,
-          "type": "조미료"
+          "type": "조미료",
+          "count": 0
        },
        {
           "name": "설탕",
           "id": 1005,
-          "type": "조미료"
+          "type": "조미료",
+          "count": 0
        },
        {
           "name": "소금",
           "id": 1006,
-          "type": "조미료"
+          "type": "조미료",
+          "count": 0
        },
        {
           "name": "참기름",
           "id": 1007,
-          "type": "조미료"
+          "type": "조미료",
+          "count": 0
        },
        {
           "name": "엿기름",
           "id": 1008,
-          "type": "조미료"
+          "type": "조미료",
+          "count": 0
        },
        {
           "name": "고춧가루",
           "id": 1009,
-          "type": "조미료"
+          "type": "조미료",
+          "count": 0
        },
        {
           "name": "케찹",
           "id": 1010,
-          "type": "조미료"
+          "type": "조미료",
+          "count": 0
        },
        {
           "name": "후추",
           "id": 1011,
-          "type": "조미료"
+          "type": "조미료",
+          "count": 0
        }
     ],
     "채소": [
        {
           "name": "쌀",
           "id": 2001,
-          "type": "곡류"
+          "type": "곡류",
+          "count": 0
        },
        {
           "name": "아몬드",
           "id": 2002,
-          "type": "곡류"
+          "type": "곡류",
+          "count": 0
        },
        {
           "name": "은행",
           "id": 2003,
-          "type": "곡류"
+          "type": "곡류",
+          "count": 0
        },
        {
           "name": "양파",
           "id": 2101,
-          "type": "야채"
+          "type": "야채",
+          "count": 0
        },
        {
           "name": "당근",
           "id": 2102,
-          "type": "야채"
+          "type": "야채",
+          "count": 0
        },
        {
           "name": "단호박",
           "id": 2103,
-          "type": "야채"
+          "type": "야채",
+          "count": 0
        },
        {
           "name": "파",
           "id": 2104,
-          "type": "야채"
+          "type": "야채",
+          "count": 0
        },
        {
           "name": "마늘",
           "id": 2105,
-          "type": "야채"
+          "type": "야채",
+          "count": 0
        },
        {
           "name": "무",
           "id": 2106,
-          "type": "야채"
+          "type": "야채",
+          "count": 0
        },
        {
           "name": "고사리",
           "id": 2107,
-          "type": "야채"
+          "type": "야채",
+          "count": 0
        },
        {
           "name": "시금치",
           "id": 2108,
-          "type": "야채"
+          "type": "야채",
+          "count": 0
        },
        {
           "name": "감자",
           "id": 2109,
-          "type": "야채"
+          "type": "야채",
+          "count": 0
        },
        {
           "name": "청양고추",
           "id": 2110,
-          "type": "야채"
+          "type": "야채",
+          "count": 0
        },
        {
           "name": "애호박",
           "id": 2111,
-          "type": "야채"
+          "type": "야채",
+          "count": 0
        },
        {
           "name": "배추",
           "id": 2112,
-          "type": "야채"
+          "type": "야채",
+          "count": 0
        },
        {
           "name": "오이",
           "id": 2113,
-          "type": "야채"
+          "type": "야채",
+          "count": 0
        },
        {
           "name": "사과",
           "id": 2201,
-          "type": "과일"
+          "type": "과일",
+          "count": 0
        },
        {
           "name": "배",
           "id": 2202,
-          "type": "과일"
+          "type": "과일",
+          "count": 0
        },
        {
           "name": "표고버섯",
           "id": 2301,
-          "type": "버섯"
+          "type": "버섯",
+          "count": 0
        },
        {
           "name": "팽이버섯",
           "id": 2302,
-          "type": "버섯"
+          "type": "버섯",
+          "count": 0
        }
     ],
     "육류": [
        {
           "name": "돼지고기",
           "id": 3001,
-          "type": "육류"
+          "type": "육류",
+          "count": 0
        },
        {
           "name": "소고기",
           "id": 3002,
-          "type": "육류"
+          "type": "육류",
+          "count": 0
        },
        {
           "name": "닭고기",
           "id": 3003,
-          "type": "육류"
+          "type": "육류",
+          "count": 0
        },
        {
           "name": "계란",
           "id": 3004,
-          "type": "육류"
+          "type": "육류",
+          "count": 0
        }
     ],
     "해산물": [
        {
           "name": "붕어",
           "id": 4001,
-          "type": "민물"
+          "type": "민물",
+          "count": 0
        },
        {
           "name": "우럭",
           "id": 4101,
-          "type": "바다"
+          "type": "바다",
+          "count": 0
        },
        {
           "name": "바지락",
           "id": 4102,
-          "type": "바다"
+          "type": "바다",
+          "count": 0
        }
     ],
     "공산품": [
        {
           "name": "가래떡",
           "id": 5001,
-          "type": "공산품"
+          "type": "공산품",
+          "count": 0
        },
        {
           "name": "어묵",
           "id": 5002,
-          "type": "공산품"
+          "type": "공산품",
+          "count": 0
        },
        {
           "name": "햄",
           "id": 5003,
-          "type": "공산품"
+          "type": "공산품",
+          "count": 0
        },
        {
           "name": "소시지",
           "id": 5004,
-          "type": "공산품"
+          "type": "공산품",
+          "count": 0
        },
        {
           "name": "칼국수면",
           "id": 5005,
-          "type": "공산품"
+          "type": "공산품",
+          "count": 0
        },
        {
           "name": "라면",
           "id": 5006,
-          "type": "공산품"
+          "type": "공산품",
+          "count": 0
        },
        {
           "name": "김",
           "id": 5007,
-          "type": "공산품"
+          "type": "공산품",
+          "count": 0
        },
        {
           "name": "단무지",
           "id": 5008,
-          "type": "공산품"
+          "type": "공산품",
+          "count": 0
        },
        {
           "name": "단팥",
           "id": 5009,
-          "type": "공산품"
+          "type": "공산품",
+          "count": 0
        },
        {
           "name": "우유",
           "id": 5010,
-          "type": "공산품"
+          "type": "공산품",
+          "count": 0
        },
        {
           "name": "시리얼",
           "id": 5011,
-          "type": "공산품"
+          "type": "공산품",
+          "count": 0
        },
        {
           "name": "젤리",
           "id": 5012,
-          "type": "공산품"
+          "type": "공산품",
+          "count": 0
        },
        {
           "name": "연유",
           "id": 5013,
-          "type": "공산품"
+          "type": "공산품",
+          "count": 0
        },
        {
           "name": "김가루",
           "id": 5014,
-          "type": "공산품"
+          "type": "공산품",
+          "count": 0
        },
        {
           "name": "식용유",
           "id": 5015,
-          "type": "공산품"
+          "type": "공산품",
+          "count": 0
        }
     ]
  }
